9f6bf342c5af11b5f568c0cb915d0c5de8cfafba
boyuanx <boyuanxu@usc.edu>
2024-02-11 01:12:34 -0800

    Update file structure

--- src/deployer.ts
+++ src/deployer.ts
@@ -4,7 +4,7 @@ import { TokenTableSuiteDeployed } from "../generated/schema";
 export function handleTokenTableSuiteDeployed(
   event: TokenTableSuiteDeployedEvent
 ): void {
-  let entity = new TokenTableSuiteDeployed(event.data[1]);
+  let entity = new TokenTableSuiteDeployed(event.data[1]); // projectId
   entity.from = event.data[0];
   entity.projectToken = event.data[2];
   entity.unlocker = event.data[3];
